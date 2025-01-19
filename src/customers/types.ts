9a6244bcb0148531d8bf18f765031c55095a1a42
Lucas Quadros <1lucascq@gmail.com>
2025-01-19 20:36:38 -0300

    refactor: rename interfaces.ts to types.ts and simplify findOne method signature in ICustomersRepository

--- src/customers/types.ts
+++ src/customers/types.ts
@@ -3,9 +3,7 @@ import { Prisma, Customer } from '@prisma/client';
 export interface ICustomersRepository {
     create(createCustomerDto: Prisma.CustomerCreateInput): Promise<Customer>;
     findAll(): Promise<Customer[]>;
-    findOne(
-        uniqueInput: Prisma.CustomerWhereUniqueInput,
-    ): Promise<Customer | null>;
+    findOne(uniqueInput: Prisma.CustomerWhereUniqueInput): Promise<Customer | null>;
     update(email: string, data: Prisma.CustomerUpdateInput): Promise<Customer>;
     delete(email: string): Promise<Customer>;
 }
