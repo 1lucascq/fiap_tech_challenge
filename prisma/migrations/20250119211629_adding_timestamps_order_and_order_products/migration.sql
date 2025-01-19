6e9594ac9be1f7794b22d3ef41d6952358d29c3d
Lucas Quadros <1lucascq@gmail.com>
2025-01-19 20:36:45 -0300

    feat: add migration for timestamps and restructure Order, Customer, and Product tables

---
+++ prisma/migrations/20250119211629_adding_timestamps_order_and_order_products/migration.sql
@@ -0,0 +1,68 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `content` on the `Order` table. All the data in the column will be lost.
+  - You are about to drop the column `customerCpf` on the `Order` table. All the data in the column will be lost.
+  - You are about to drop the column `customerName` on the `Order` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `Customer` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `customerId` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `status` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `total` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateTable
+CREATE TABLE "OrderProduct" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "orderId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 1,
+    CONSTRAINT "OrderProduct_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "OrderProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Customer" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Customer" ("cpf", "email", "id", "name") SELECT "cpf", "email", "id", "name" FROM "Customer";
+DROP TABLE "Customer";
+ALTER TABLE "new_Customer" RENAME TO "Customer";
+CREATE UNIQUE INDEX "Customer_email_key" ON "Customer"("email");
+CREATE UNIQUE INDEX "Customer_cpf_key" ON "Customer"("cpf");
+CREATE TABLE "new_Order" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "customerId" INTEGER NOT NULL,
+    "total" REAL NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Order_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Order" ("id") SELECT "id" FROM "Order";
+DROP TABLE "Order";
+ALTER TABLE "new_Order" RENAME TO "Order";
+CREATE TABLE "new_Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "ingredients" TEXT,
+    "categoryId" INTEGER NOT NULL,
+    "price" REAL NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Product" ("categoryId", "id", "ingredients", "name", "price") SELECT "categoryId", "id", "ingredients", "name", "price" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
